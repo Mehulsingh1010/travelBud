764bc7fca3c8fa6810adf3d2ded48ae70b1102ef
Kislay0 <kislay.kt18@gmail.com>
2025-09-03 13:07:41 +0530

    mehul's sidebar, dp changed to mine.

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,6 +5,6 @@ import * as paymentSchema from "./payments"
 const schema = { ...centralSchema, ...paymentSchema }
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_xTpfcLlk23id@ep-odd-leaf-a8kkpjio-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
+  connectionString: 'postgresql://neondb_owner:npg_BJqirS9kYEH8@ep-dark-forest-a113njrf-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 });
 export const db = drizzle(pool, { schema })
